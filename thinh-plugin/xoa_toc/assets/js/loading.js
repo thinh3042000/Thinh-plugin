535adad7df1c4a027deb39731c99b544b36c238e
Thinh Kun <thinh3042000@gmail.com>
2024-07-22 08:26:53 +0700

    develop plugin

--- thinh-plugin/xoa_toc/assets/js/loading.js
+++ thinh-plugin/xoa_toc/assets/js/loading.js
@@ -7,3 +7,4 @@ document.addEventListener("DOMContentLoaded", function() {
         tocLoading.style.display = 'none';
     }
 });
+
--- thinh-plugin/xoa_toc/includes/xoa_toc_plugin.php
+++ thinh-plugin/xoa_toc/includes/xoa_toc_plugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WP_xoa_TOC;
 
 if (!defined('ABSPATH')) {
@@ -7,13 +8,12 @@ if (!defined('ABSPATH')) {
 
 final class XoaTocPlugin
 {
- 
+
     private static $_instance = null;
 
     public static function instance()
     {
-        if ( is_null( self::$_instance) )
-        {
+        if (is_null(self::$_instance)) {
             self::$_instance = new self();
         }
         return self::$_instance;
@@ -21,39 +21,160 @@ final class XoaTocPlugin
 
     public function __construct()
     {
-        add_action( 'elementor/frontend/after_register_scripts', [ $this, 'frontend_scripts']);
-        add_action('init', [ $this, 'handle_shortcode']);
+        add_action('elementor/frontend/after_register_scripts', [$this, 'frontend_scripts']);
+        add_action('init', [$this, 'handle_shortcode']);
+        add_action('wp_enqueue_scripts', [$this, 'handle_css']);
     }
 
+    public function handle_css()
+    {
+        wp_enqueue_style('single-post.css', plugins_url('assets/css/single-post.css', XOA_TOC_FILE), array(), '1.0.0', 'all');
+        wp_enqueue_style('loading.css', plugins_url('assets/css/loading.css', XOA_TOC_FILE), array(), '1.0.0', 'all');
+    }
     public function frontend_scripts()
     {
         if (is_single()) {
             wp_register_script('single-post.js', plugins_url('assets/js/single-post.js', XOA_TOC_FILE), array(), null, false);
-            wp_register_style('single-post.css', plugins_url('assets/css/single-post.css', XOA_TOC_FILE), array(), null, 'all');
             wp_register_script('loading.js', plugins_url('assets/js/loading.js', XOA_TOC_FILE), array(), null, true);
-            wp_register_style('loading.css', plugins_url('assets/css/loading.css', XOA_TOC_FILE), array(), null, 'all');
             add_action('wp_head', [$this, 'enqueue_frontend_scripts'], 5);
         }
     }
+
+
     public function enqueue_frontend_scripts()
     {
         if (is_single()) {
-            wp_enqueue_style('single-post.css');
-            wp_enqueue_style('loading.css');
             wp_enqueue_script('single-post.js');
             wp_enqueue_script('loading.js');
         }
     }
-    function custom_toc_shortcode() {
+    function custom_toc_shortcode()
+    {
         global $post;
         $content = $post->post_content;
-        
+
         preg_match_all('/<h2.*?>(.*?)<\/h2>|<h3.*?>(.*?)<\/h3>/', $content, $matches);
-    
+
         if (empty($matches[0])) {
             return '';
         }
-        $toc = '<div id="toc-loading">';
+        $toc = '
+        <style> 
+        #toc-loading {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+#loading-spinner {
+    border: 4px solid #f3f3f3;
+    border-radius: 50%;
+    border-top: 4px solid #00AF5A; 
+    width: 30px; 
+    height: 30px; 
+    -webkit-animation: spin 1s linear infinite; 
+    animation: spin 1s linear infinite; 
+}
+
+@-webkit-keyframes spin {
+    0% { -webkit-transform: rotate(0deg); }
+    100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+    li.toc-item {
+  list-style: none;
+}
+
+li.toc-subitem {
+  list-style: none;
+}
+
+a.link-item-custom-child {
+  color: black;
+  font-weight: 400;
+}
+
+div#fixed-toc {
+  padding: 10px 30px;
+  border-radius: 10px;
+  border: 1px solid #979595;
+  overflow-y: scroll;
+  max-height: 350px;
+  margin-top: -40px;
+  margin-bottom: -15px;
+  border: 1px solid #e5e1e1;
+}
+div#fixed-toc-mobile {
+  padding: 2px 15px 0px 15px;
+  border-radius: 10px;
+  border: 1px solid #ffffff;
+}
+
+ul#toc-list {
+  padding: 0px;
+}
+
+a.link-item-custom {
+  color: black;
+  font-weight: 600;
+}
+
+.toc-subitem {
+  margin-left: -23px;
+}
+
+svg {
+  height: 1em;
+  width: 1em;
+  fill: var(--toggle-button-color);
+}
+span.toc-custom-mobile {
+  font-size: 16px;
+  font-weight: 400;
+}
+span.toc-custom {
+  font-size: 20px;
+  font-weight: 500;
+}
+.toc-container {
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+.toc-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: pointer;
+  margin-bottom: 15px;
+}
+.toc-toggle {
+  font-size: 1.2em;
+  display: flex;
+  align-items: center;
+}
+.toc-item a.active,
+.toc-subitem a.active {
+  color: #00af5a !important;
+}
+.toc-sublist {
+  display: none;
+}
+.toc-item.active .toc-sublist {
+  display: block;
+}
+
+
+        </style>
+        <div id="toc-loading">';
         $toc .= '<div id="loading-spinner"></div>';
         $toc .= '</div>';
         $toc .= '<div id="toc-content" style="display: none;">';
@@ -64,10 +185,10 @@ final class XoaTocPlugin
                     </svg>
                 </span></div>';
         $toc .= '<ul id="toc-list" class="toc-list" style="display: block;">';
-    
+
         $i = 0;
         $last_h2_id = '';
-    
+
         foreach ($matches[0] as $match) {
             if (strpos($match, '<h2') !== false) {
                 $i++;
@@ -75,41 +196,40 @@ final class XoaTocPlugin
                 $id = 'toc-' . $i;
                 $heading = strip_tags($match);
                 $numbered_heading = $i . '. ' . $heading;
-    
+
                 $content = str_replace($match, preg_replace('/(<h2)(.*?)(>)/', '$1 id="' . $id . '"$2$3', $match), $content);
-    
+
                 if ($last_h2_id !== '') {
                     $toc .= '</ul></li>';
                 }
-    
+
                 $toc .= '<li class="toc-item"><a class="link-item-custom" href="#' . $id . '">' . $numbered_heading . '</a>';
                 $toc .= '<ul class="toc-sublist">';
                 $last_h2_id = $id;
-    
             } elseif (strpos($match, '<h3') !== false) {
                 $j++;
                 $id = 'toc-' . $i . '-' . $j;
                 $heading = strip_tags($match);
                 $numbered_heading = $i . '.' . $j . ' ' . $heading;
-    
+
                 $content = str_replace($match, preg_replace('/(<h3)(.*?)(>)/', '$1 id="' . $id . '"$2$3', $match), $content);
-    
+
                 $toc .= '<li class="toc-subitem"><a class="link-item-custom-child" href="#' . $id . '">' . $numbered_heading . '</a></li>';
             }
         }
-    
+
         if ($last_h2_id !== '') {
             $toc .= '</ul></li>';
         }
-    
+
         $toc .= '</ul></div></div>';
-    
+
         $post->post_content = $content;
-    
+
         return $toc;
     }
-    function handle_shortcode() 
+    function handle_shortcode()
     {
-    add_shortcode('custom_toc', [$this, 'custom_toc_shortcode']);
+        add_shortcode('custom_toc', [$this, 'custom_toc_shortcode']);
     }
 }
